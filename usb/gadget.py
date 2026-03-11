34da5e02168f0bdf3f27a3c386c57be58fc2102c
Sergey004 <sergey00475@gmail.com>
2026-03-11 19:12:41 +0400

    Save

--- usb/gadget.py
+++ usb/gadget.py
@@ -267,7 +267,7 @@ class GadgetManager:
                     self._write_file(f'{lun0_path}/ro', '0')
                     self._write_file(f'{lun0_path}/cdrom', '0')
                     self._write_file(f'{lun0_path}/nofua', '1')
-                    self._write_file(f'{lun0_path}/inquiry_string', 'ZeroCD USB Flash Drive')
+                    self._write_file(f'{lun0_path}/inquiry_string', 'ZeroCD  USB Flash Drive')
 
                 os.symlink(ms_path, f'{config_path}/mass_storage.usb0')
 
